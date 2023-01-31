62d92a451032493def6e26cc99cce836bbdb8780
kd185171 <kd185171@ncr.com>
2023-01-31 12:51:18 +0530

    weeklydata updated

--- src/Components/Chart1 Components/Statistics Components/Chart1.tsx
+++ src/Components/Chart1 Components/Statistics Components/Chart1.tsx
@@ -24,11 +24,8 @@ import NavBar from '../Navbar1';
 import Compare from '../Compare Components/compare';
 import { useMountedLayoutEffect } from 'react-table';
 import axios from 'axios';
-<<<<<<< HEAD
 import { FaDailymotion } from 'react-icons/fa';
-=======
 import ReactDOM from 'react-dom';
->>>>>>> 7446fe0e7a5fdc631afb894d13a7b8f24454c18f
 
 
 
@@ -76,7 +73,6 @@ const Chart1 = (props: { orderList: AppState[] }) => {
   const [isCompare, setCompare] = useState(true);
 
   const data1: AppState[] = props.orderList;
-<<<<<<< HEAD
 
   let monthly: Monthly[] = [];
 
@@ -116,10 +112,8 @@ const Chart1 = (props: { orderList: AppState[] }) => {
 
 
 
-=======
 
 
->>>>>>> 7446fe0e7a5fdc631afb894d13a7b8f24454c18f
   var first15 = data1.filter(function (el, index) {
     return index >= data1.length - 15;
   });
@@ -156,11 +150,9 @@ const Chart1 = (props: { orderList: AppState[] }) => {
     console.log('called update function')
     update(30)
   }, [props.orderList]);
-<<<<<<< HEAD
 
   function update(num: Number) {
     if(flag==1){
-=======
 
   const [is865,Set865] = useState(false);
   const [is365,Set365] = useState(false);
@@ -179,7 +171,6 @@ const Chart1 = (props: { orderList: AppState[] }) => {
     Set365(false);
     Set865(false);
 
->>>>>>> 7446fe0e7a5fdc631afb894d13a7b8f24454c18f
     switch (num) {
       case 15:
         Set15(true);
@@ -400,198 +391,193 @@ const Chart1 = (props: { orderList: AppState[] }) => {
         break;
     }
   }
-  if(flag==1){
-    switch (num) {
-        case 30:
-          monthly.length = 0;
-          {
-            monthly.push(finalResult[0]);
-          }
-          var h = {
-            labels: monthly.map((item) => item.Month),
-            datasets: [
-              {
-                label: 'AttemptedOrders',
-                data: monthly.map((item) => item.AttemptedOrders),
-                borderColor: '#FA8231',
-                backgroundColor: '#FA8231',
-              },
-              {
-                label: 'CompletedOrders',
-                data: monthly.map((item) => item.CompletedOrders),
-                borderColor: '#3C40C6',
-                backgroundColor: '#3C40C6',
-              },
-              {
-                label: 'TotalOrders',
-                data: monthly.map((item) => item.TotalOrders),
-                borderColor: '#55B74E',
-                backgroundColor: '#55B74E',
-              },
-            ],
-          }
-          setMonth(h);
-          break;
-      case 60:
-        monthly.length = 0;
-       for(var z=0;z<=1;z++) {
-          monthly.push(finalResult[z]);
-        }
-        var i = {
-          labels: monthly.map((item) => item.Month),
-          datasets: [
-            {
-              label: 'AttemptedOrders',
-              data: monthly.map((item) => item.AttemptedOrders),
-              borderColor: '#FA8231',
-              backgroundColor: '#FA8231',
-            },
-            {
-              label: 'CompletedOrders',
-              data: monthly.map((item) => item.CompletedOrders),
-              borderColor: '#3C40C6',
-              backgroundColor: '#3C40C6',
-            },
-            {
-              label: 'TotalOrders',
-              data: monthly.map((item) => item.TotalOrders),
-              borderColor: '#55B74E',
-              backgroundColor: '#55B74E',
-            },
-          ],
-        }
-        setMonth(i);
-        break;
-      case 90:
-        monthly.length = 0;
-        for(var z=0;z<=2;z++) {
-          monthly.push(finalResult[z]);
-        }
-        var j = {
-          labels: monthly.map((item) => item.Month),
-          datasets: [
-            {
-              label: 'AttemptedOrders',
-              data: monthly.map((item) => item.AttemptedOrders),
-              borderColor: '#FA8231',
-              backgroundColor: '#FA8231',
-            },
-            {
-              label: 'CompletedOrders',
-              data: monthly.map((item) => item.CompletedOrders),
-              borderColor: '#3C40C6',
-              backgroundColor: '#3C40C6',
-            },
-            {
-              label: 'TotalOrders',
-              data: monthly.map((item) => item.TotalOrders),
-              borderColor: '#55B74E',
-              backgroundColor: '#55B74E',
-            },
-          ],
-        }
-        setMonth(j);
-        break;
-      case 180:
-        monthly.length = 0;
-        for(var z=0;z<=5;z++) {
-          monthly.push(finalResult[z]);
-        }
-        var k = {
-          labels: monthly.map((item) => item.Month),
-          datasets: [
-            {
-              label: 'AttemptedOrders',
-              data: monthly.map((item) => item.AttemptedOrders),
-              borderColor: '#FA8231',
-              backgroundColor: '#FA8231',
-            },
-            {
-              label: 'CompletedOrders',
-              data: monthly.map((item) => item.CompletedOrders),
-              borderColor: '#3C40C6',
-              backgroundColor: '#3C40C6',
-            },
-            {
-              label: 'TotalOrders',
-              data: monthly.map((item) => item.TotalOrders),
-              borderColor: '#55B74E',
-              backgroundColor: '#55B74E',
-            },
-          ],
-        }
-        setMonth(k);
-        break;
-      case 365:
-        monthly.length = 0;
-        for(var z=0;z<=11;z++) {
-          monthly.push(finalResult[z]);
-        }
-        var l = {
-          labels: monthly.map((item) => item.Month),
-          datasets: [
-            {
-              label: 'AttemptedOrders',
-              data: monthly.map((item) => item.AttemptedOrders),
-              borderColor: '#FA8231',
-              backgroundColor: '#FA8231',
-            },
-            {
-              label: 'CompletedOrders',
-              data: monthly.map((item) => item.CompletedOrders),
-              borderColor: '#3C40C6',
-              backgroundColor: '#3C40C6',
-            },
-            {
-              label: 'TotalOrders',
-              data: monthly.map((item) => item.TotalOrders),
-              borderColor: '#55B74E',
-              backgroundColor: '#55B74E',
-            },
-          ],
-        }
-        setMonth(l);
-        break;
-      case 856:
-        Ddata.length = 0;
-        for(var z=0;z<=finalResult.length-1;z++) {
-          monthly.push(finalResult[z]);
-        }
-        var m = {
-          labels: monthly.map((item) => item.Month),
-          datasets: [
-            {
-              label: 'AttemptedOrders',
-              data: monthly.map((item) => item.AttemptedOrders),
-              borderColor: '#FA8231',
-              backgroundColor: '#FA8231',
-            },
-            {
-              label: 'CompletedOrders',
-              data: monthly.map((item) => item.CompletedOrders),
-              borderColor: '#3C40C6',
-              backgroundColor: '#3C40C6',
-            },
-            {
-              label: 'TotalOrders',
-              data: monthly.map((item) => item.TotalOrders),
-              borderColor: '#55B74E',
-              backgroundColor: '#55B74E',
-            },
-          ],
-        }
-        setMonth(m);
-        break;
-    }
+  // if(flag==1){
+  //   switch (num) {
+  //       case 30:
+  //         monthly.length = 0;
+  //         {
+  //           monthly.push(finalResult[0]);
+  //         }
+  //         var h = {
+  //           labels: monthly.map((item) => item.Month),
+  //           datasets: [
+  //             {
+  //               label: 'AttemptedOrders',
+  //               data: monthly.map((item) => item.AttemptedOrders),
+  //               borderColor: '#FA8231',
+  //               backgroundColor: '#FA8231',
+  //             },
+  //             {
+  //               label: 'CompletedOrders',
+  //               data: monthly.map((item) => item.CompletedOrders),
+  //               borderColor: '#3C40C6',
+  //               backgroundColor: '#3C40C6',
+  //             },
+  //             {
+  //               label: 'TotalOrders',
+  //               data: monthly.map((item) => item.TotalOrders),
+  //               borderColor: '#55B74E',
+  //               backgroundColor: '#55B74E',
+  //             },
+  //           ],
+  //         }
+  //         setMonth(h);
+  //         break;
+  //     case 60:
+  //       monthly.length = 0;
+  //      for(var z=0;z<=1;z++) {
+  //         monthly.push(finalResult[z]);
+  //       }
+  //       var i = {
+  //         labels: monthly.map((item) => item.Month),
+  //         datasets: [
+  //           {
+  //             label: 'AttemptedOrders',
+  //             data: monthly.map((item) => item.AttemptedOrders),
+  //             borderColor: '#FA8231',
+  //             backgroundColor: '#FA8231',
+  //           },
+  //           {
+  //             label: 'CompletedOrders',
+  //             data: monthly.map((item) => item.CompletedOrders),
+  //             borderColor: '#3C40C6',
+  //             backgroundColor: '#3C40C6',
+  //           },
+  //           {
+  //             label: 'TotalOrders',
+  //             data: monthly.map((item) => item.TotalOrders),
+  //             borderColor: '#55B74E',
+  //             backgroundColor: '#55B74E',
+  //           },
+  //         ],
+  //       }
+  //       setMonth(i);
+  //       break;
+  //     case 90:
+  //       monthly.length = 0;
+  //       for(var z=0;z<=2;z++) {
+  //         monthly.push(finalResult[z]);
+  //       }
+  //       var j = {
+  //         labels: monthly.map((item) => item.Month),
+  //         datasets: [
+  //           {
+  //             label: 'AttemptedOrders',
+  //             data: monthly.map((item) => item.AttemptedOrders),
+  //             borderColor: '#FA8231',
+  //             backgroundColor: '#FA8231',
+  //           },
+  //           {
+  //             label: 'CompletedOrders',
+  //             data: monthly.map((item) => item.CompletedOrders),
+  //             borderColor: '#3C40C6',
+  //             backgroundColor: '#3C40C6',
+  //           },
+  //           {
+  //             label: 'TotalOrders',
+  //             data: monthly.map((item) => item.TotalOrders),
+  //             borderColor: '#55B74E',
+  //             backgroundColor: '#55B74E',
+  //           },
+  //         ],
+  //       }
+  //       setMonth(j);
+  //       break;
+  //     case 180:
+  //       monthly.length = 0;
+  //       for(var z=0;z<=5;z++) {
+  //         monthly.push(finalResult[z]);
+  //       }
+  //       var k = {
+  //         labels: monthly.map((item) => item.Month),
+  //         datasets: [
+  //           {
+  //             label: 'AttemptedOrders',
+  //             data: monthly.map((item) => item.AttemptedOrders),
+  //             borderColor: '#FA8231',
+  //             backgroundColor: '#FA8231',
+  //           },
+  //           {
+  //             label: 'CompletedOrders',
+  //             data: monthly.map((item) => item.CompletedOrders),
+  //             borderColor: '#3C40C6',
+  //             backgroundColor: '#3C40C6',
+  //           },
+  //           {
+  //             label: 'TotalOrders',
+  //             data: monthly.map((item) => item.TotalOrders),
+  //             borderColor: '#55B74E',
+  //             backgroundColor: '#55B74E',
+  //           },
+  //         ],
+  //       }
+  //       setMonth(k);
+  //       break;
+  //     case 365:
+  //       monthly.length = 0;
+  //       for(var z=0;z<=11;z++) {
+  //         monthly.push(finalResult[z]);
+  //       }
+  //       var l = {
+  //         labels: monthly.map((item) => item.Month),
+  //         datasets: [
+  //           {
+  //             label: 'AttemptedOrders',
+  //             data: monthly.map((item) => item.AttemptedOrders),
+  //             borderColor: '#FA8231',
+  //             backgroundColor: '#FA8231',
+  //           },
+  //           {
+  //             label: 'CompletedOrders',
+  //             data: monthly.map((item) => item.CompletedOrders),
+  //             borderColor: '#3C40C6',
+  //             backgroundColor: '#3C40C6',
+  //           },
+  //           {
+  //             label: 'TotalOrders',
+  //             data: monthly.map((item) => item.TotalOrders),
+  //             borderColor: '#55B74E',
+  //             backgroundColor: '#55B74E',
+  //           },
+  //         ],
+  //       }
+  //       setMonth(l);
+  //       break;
+  //     case 856:
+  //       Ddata.length = 0;
+  //       for(var z=0;z<=finalResult.length-1;z++) {
+  //         monthly.push(finalResult[z]);
+  //       }
+  //       var m = {
+  //         labels: monthly.map((item) => item.Month),
+  //         datasets: [
+  //           {
+  //             label: 'AttemptedOrders',
+  //             data: monthly.map((item) => item.AttemptedOrders),
+  //             borderColor: '#FA8231',
+  //             backgroundColor: '#FA8231',
+  //           },
+  //           {
+  //             label: 'CompletedOrders',
+  //             data: monthly.map((item) => item.CompletedOrders),
+  //             borderColor: '#3C40C6',
+  //             backgroundColor: '#3C40C6',
+  //           },
+  //           {
+  //             label: 'TotalOrders',
+  //             data: monthly.map((item) => item.TotalOrders),
+  //             borderColor: '#55B74E',
+  //             backgroundColor: '#55B74E',
+  //           },
+  //         ],
+  //       }
+  //       setMonth(m);
+  //       break;
+  //   }
 
-  }
+  // }
 
-<<<<<<< HEAD
-=======
-  for (var i = 0; i < first30.length; i++) {
-    Ddata.push(first30[i]);
->>>>>>> 7446fe0e7a5fdc631afb894d13a7b8f24454c18f
-  }
+
 
 
 
@@ -621,7 +607,6 @@ const Chart1 = (props: { orderList: AppState[] }) => {
     ],
   });
 
-<<<<<<< HEAD
   var [month, setMonth] = useState({
     labels: monthly.reverse().map((item) => item.Month),
     datasets: [
@@ -646,33 +631,31 @@ const Chart1 = (props: { orderList: AppState[] }) => {
     ],
   });
 
-  var [week, setWeek] = useState({
-    labels: monthly.reverse().map((item) => item.Month),
-    datasets: [
-      {
-        label: 'AttemptedOrders',
-        data: monthly.map((item) => item.AttemptedOrders),
-        borderColor: '#FA8231',
-        backgroundColor: '#FA8231',
-      },
-      {
-        label: 'CompletedOrders',
-        data: monthly.map((item) => item.CompletedOrders),
-        borderColor: '#3C40C6',
-        backgroundColor: '#3C40C6',
-      },
-      {
-        label: 'TotalOrders',
-        data: monthly.map((item) => item.TotalOrders),
-        borderColor: '#55B74E',
-        backgroundColor: '#55B74E',
-      },
-    ],
-  });
+  // var [week, setWeek] = useState({
+  //   labels: monthly.reverse().map((item) => item.Month),
+  //   datasets: [
+  //     {
+  //       label: 'AttemptedOrders',
+  //       data: monthly.map((item) => item.AttemptedOrders),
+  //       borderColor: '#FA8231',
+  //       backgroundColor: '#FA8231',
+  //     },
+  //     {
+  //       label: 'CompletedOrders',
+  //       data: monthly.map((item) => item.CompletedOrders),
+  //       borderColor: '#3C40C6',
+  //       backgroundColor: '#3C40C6',
+  //     },
+  //     {
+  //       label: 'TotalOrders',
+  //       data: monthly.map((item) => item.TotalOrders),
+  //       borderColor: '#55B74E',
+  //       backgroundColor: '#55B74E',
+  //     },
+  //   ],
+  // });
 
 
-=======
->>>>>>> 7446fe0e7a5fdc631afb894d13a7b8f24454c18f
 
   function RenderChart() {
     return (
@@ -723,14 +706,11 @@ const Chart1 = (props: { orderList: AppState[] }) => {
     setCompare(true);
     setVisible(true);
   }
-  function Daily() {
-    update(30);
-    flag = 1;
-  }
+  
 
   
  
-  function RenderWeekChart() {
+  function RenderChart() {
     return (
       <div className="container">
         <Line className="Element" options={options} data={month} />
@@ -748,28 +728,6 @@ const Chart1 = (props: { orderList: AppState[] }) => {
     <div>
       <NavBar />
       <div id='btn-grp'>
-<<<<<<< HEAD
-        <button onClick={() => { StatisticsPage() }} type="button" id='btns' className="btn btn-outline-primary btn-lg" >Statistics</button>
-        <button onClick={() => { ComparePage() }} type="button" id='btns' className="btn btn-outline-primary btn-lg">Compare</button>
-        <div id='btns2'>
-        {visible && <button onClick={() =>  RenderMonthChart() } type="button" id='btns' className="btn btn-outline-primary btn-sm" >Monthly</button>}
-        {visible && <button onClick={() =>  RenderWeekChart() } type="button" id='btns' className="btn btn-outline-primary btn-sm">Weekly</button>}
-        {visible && <button onClick={() => RenderChart()} type="button" id='btns' className="btn btn-outline-primary btn-sm" >Daily</button>}
-        </div>
-        <div />
-        {isCompare ? (isBar ? <RenderBarChart /> : <RenderChart />) : <Compare orderList={data1} />}
-        <div>
-          {visible && <button onClick={() => SetBar(false)} type="button" id='btns1' className="btn btn-outline-primary btn-sm">Line-Chart</button>}
-          {visible && <button onClick={() => SetBar(true)} type="button" id='btns1' className="btn btn-outline-primary btn-sm">Bar-Chart</button>}</div>
-        <div id="btn-grp2">
-          {visible && <button onClick={() => update(856)} type="button" id='btns1' className="btn btn-outline-primary btn-sm">856 Days</button>}
-          {visible && <button onClick={() => update(365)} type="button" id='btns1' className="btn btn-outline-primary btn-sm">365 Days</button>}
-          {visible && <button onClick={() => update(180)} type="button" id='btns1' className="btn btn-outline-primary btn-sm">180 Days</button>}
-          {visible && <button onClick={() => update(90)} type="button" id='btns1' className="btn btn-outline-primary btn-sm">90 Days</button>}
-          {visible && <button onClick={() => update(60)} type="button" id='btns1' className="btn btn-outline-primary btn-sm">60 Days</button>}
-          {visible && <button onClick={() => update(30)} type="button" id='btns11' className="btn btn-outline-primary btn-sm">30 Days</button>}
-          {visible && <button onClick={() => update(15)} type="button" id='btns1' className="btn btn-outline-primary btn-sm">15 Days</button>}
-=======
         <button onClick={() => { StatisticsPage() }}  id='btns' style={{backgroundColor: isCompare ? '#55B74E' : '',color: isCompare ? 'white' : ''}} className={c_class} >Statistics</button>
         <button onClick={() => { ComparePage() }} id='btns' style={{backgroundColor: !isCompare ? '#55B74E' : '',color: !isCompare ? 'white' : ''}} className= {c_class}>Compare</button>
         <div />
@@ -785,7 +743,6 @@ const Chart1 = (props: { orderList: AppState[] }) => {
           {visible && <button onClick={() => update(60)} id='btns1' style={{backgroundColor: is60 ? '#55B74E' : '',color: is60 ? 'white' : ''}} className={c1_class}>60 Days</button>}
           {visible && <button onClick={() => update(30)} id='btns1' style={{backgroundColor: is30 ? '#55B74E' : '',color: is30 ? 'white' : ''}} className={c1_class}>30 Days</button>}
           {visible && <button onClick={() => update(15)} id='btns1' style={{backgroundColor: is15 ? '#55B74E' : '',color: is15 ? 'white' : ''}} className={c1_class}>15 Days</button>}
->>>>>>> 7446fe0e7a5fdc631afb894d13a7b8f24454c18f
         </div>
       </div>
     </div>
